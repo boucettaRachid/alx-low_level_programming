27a3e26efa93a9ec8f1a52166df840d15bda1ab7
rachid bouctta <46158710+boucettaRachid@users.noreply.github.com>
2023-03-22 09:09:03 +0000

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,8 +6,19 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void)
-{
+int main() {
+    int count = 98;
+    int first = 1, second = 2;
+    int next;
 
-	return (0);
+    printf("%d\n%d\n", first, second);
+
+    for (int i = 2; i < count; i++) {
+        next = first + second;
+        printf("%d\n", next);
+        first = second;
+        second = next;
+    }
+
+    return 0;
 }
