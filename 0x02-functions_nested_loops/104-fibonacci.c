fff0963a0376c0d76df8de689ec17f34bcd99375
rachid bouctta <46158710+boucettaRachid@users.noreply.github.com>
2023-03-22 10:22:10 +0000

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,29 +6,6 @@
  *
  * Return: Always 0 (Success)
  */
-void print_fib(int len)
-{
-	unsigned int target, i;
-
-	unsigned int former_l, former_r,;
-	unsigned int latter_l, latter_r;
-	unsigned int fib_l, fib_r;
-
-	f = 1;
-	l = 2;
-	target = len - 2;
-	for (i = 0 ; i < target ; i++)
-	{
-		fib = f + l;
-		printf("%d : ", i + 4);
-		printf("%.0Lf" , fib);
-		f = l;
-		l = fib;
-		if (i < target - 1)
-			printf("\n");
-	}
-	putchar('\n');
-}
 
 int main(void)
 {
